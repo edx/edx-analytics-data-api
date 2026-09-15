a4d014923538ea1c6387fd5baafbbf3135849638
Santhosh Kumar <santhosh.kumar@apphelix.ai>
2026-09-15 12:39:41 +0530

    test: verify admin requires authentication

--- analyticsdataserver/tests/test_urls.py
+++ analyticsdataserver/tests/test_urls.py
@@ -5,3 +5,8 @@ from django.urls import reverse
 class UrlConfigurationTests(SimpleTestCase):
     def test_admin_url_is_exposed(self):
         self.assertEqual(reverse('admin:index'), '/admin/')
+
+    def test_admin_requires_authentication(self):
+        response = self.client.get(reverse('admin:index'))
+
+        self.assertRedirects(response, '/admin/login/?next=/admin/')
